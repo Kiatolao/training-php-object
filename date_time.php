1af638a79355ede9faff0cbf65fc58b1173f3a10
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-27 00:31:27 +0200

    Update date_time.php

--- date_time.php
+++ date_time.php
@@ -1,4 +1,7 @@
 <?php
+
+//creation d'un nouvel objet || $x-> declare une methode (modify(), format()...)
 $date = new Datetime();
+$date->modify('+1month');
 echo $date-> format('d/m/Y');
 var_dump($date);
