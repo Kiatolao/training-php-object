ce4e534a043d0ba1e8d3f82caff107129ff2fb2c
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-29 00:59:13 +0200

    Create Compteur.php

---
+++ class/Compteur.php
@@ -0,0 +1,16 @@
+<?php
+
+class Compteur {
+    public $fichier;
+
+    public function __construct(string $fichier) 
+    {
+        $this->fichier = $fichier;
+    }
+
+    public function incrementer() 
+    {
+
+    }
+
+}
