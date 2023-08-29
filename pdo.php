3cf034462047e3936ca38f7600bf13eea6c07036
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-29 23:35:03 +0200

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,3 +1,16 @@
 <?php
-$pdo = new PDO('sqlite:/data.db');
-$pdo->query('SELECT * FROM posts');
+$pdo = new PDO('sqlite:data.db');
+$query = $pdo->query('SELECT * FROM posts');
+if($query === false) {
+    var_dump($pdo->errorInfo());
+    die('Erreur SQL');
+}
+$posts = $query->fetchAll(PDO::FETCH_OBJ);
+print_r($posts);
+echo '</pre>';
+?>
+
+<ul><?php foreach($posts as $post) :?>
+    <li><?= $post->title?></li>
+    <?php endforeach?>
+</ul>
