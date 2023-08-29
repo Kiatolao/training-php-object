00918d0b15e693cbfda23eb1b0b2dfb10890f3bb
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-29 16:58:20 +0200

    Update api.php

--- api.php
+++ api.php
@@ -5,11 +5,24 @@ $data = curl_exec($curl);
 //permet de desactiver la verification SSL - mauvaise pratique
 //curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 //Methode clean - recupérer le certificat via le cadenas dans le navigateur et executer:
-// curl_setopt($curl, CURLOPT_CAINFO, 'path');
+curl_setopt_array(
+    $curl, [
+        CURLOPT_CAINFO  => 'path',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 1    
+
+    ]
+);
 
 if ($data === false) {
     var_dump(curl_error($curl));
 } else {
-
-}
+    if (curl_getinfo($curl, CURLINFO_HTTP_CODE) === 200){
+        $data = json_decode($data, true);
+        echo '<pre>';
+            var_dump($data['current_weather']['temperature']);
+        echo '</pre>';
+    }
+    }
+  
 curl_close($curl);
