29a70c1a637510b65da4bad139bfe908c07dac1a
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-08-31 03:51:14 +0200

    Update librairies_tierces.php

--- librairies_tierces.php
+++ librairies_tierces.php
@@ -9,4 +9,12 @@ exemple pour une librairie markdown
 php composer require erusev/parsedown
 
 bien lire la doc pour les question de sécurité
+
+Lib utilies
+erusev/parsedown
+symfony/var_dumper
+
+Il est possible d update les libs
+php composer update
+Attention au contraintes
 */
