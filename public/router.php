bbd59b28074d9bf67270307a50e8e51bdd06299e
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-09-01 17:52:24 +0200

    Update router.php

--- public/router.php
+++ public/router.php
@@ -1,18 +1,17 @@
 <?php
 require "../vendor/autoload.php";
 require "../elements/header.php";
-$page = $_GET["page"] ?? "404";
- if ($page === 'pdo') {
-    require "../pages/pdo.php";
- } else if ($page === '404') {
-    require "../pages/404.php";
- }
+$uri = $_SERVER['REQUEST_URI'];
+if ($uri['REQUEST_URI'] === '/nous-contacter') {
+    require '../templates/contact.php';
+} elseif ($uri['REQUEST_URI'] === '/') {
+    require '../templates/home.php';
+} else {
+    http_response_code(404);
+    echo "404";
+}  
  require "../elements/footer.php";
 ?>
-
-<h1>Bienvenue</h1>
-
-
 
 <!-- pour changer le dossier par defaut dans public
 php -S localhost:8000 -t public
