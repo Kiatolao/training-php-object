1478c00afe4940add48a17656da2400ddae64f27
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-09-01 18:10:54 +0200

    Update router.php

--- public/router.php
+++ public/router.php
@@ -1,6 +1,6 @@
 <?php
 require "../vendor/autoload.php";
-require "../elements/header.php";
+
 $uri = $_SERVER['REQUEST_URI'];
 // altorouter extension doit être instancié
 $router = new AltoRouter();
@@ -10,7 +10,16 @@ $router->map('GET', '/', function () {
 $router->map('GET', '/nous-contacter', function () {
     echo "Nous contacter";}
 );
- require "../elements/footer.php";
+$router->map('GET', '/blog/[*:slug]-[i:id]', function ($slug, $id) {
+    echo "Je suis l'article $slug et mon id est $id";}
+);
+$match = $router->match();
+if ($match !== false) {
+    require "../elements/header.php";
+    call_user_func_array($match['target'], $match['params']);
+    require "../elements/footer.php";
+};
+ 
 ?>
 
 <!-- pour changer le dossier par defaut dans public
